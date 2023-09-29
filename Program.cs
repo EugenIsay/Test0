bbd8ccffd852462a6bb71a2831b30ef55b18a575
MaksimK30 <makskuzmih@mail.ru>
2023-09-29 14:39:30 +0300

    Code redone

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿Console.WriteLine($"Введите количество интервалов");
+﻿start:
+Console.WriteLine($"Введите количество интервалов");
 int N;
 N = Convert.ToInt32(Console.ReadLine());
+if (N > 500 || N < 1)
+{
+    Console.Clear();
+    Console.WriteLine($"Ошибка ввода");
+    goto start;
+}
 int[] array0 = new int[(int)Math.Pow(10, 9)];
-int count = 0;
-int[,] array1 = new int[2, N];
-Random rand = new Random();
+int count = 0;int x; int y; int temp = 0;
+//Random rand = new Random();
 //for (int i = 0; i < N; i++)
 //{
 //    array1[0, i] = rand.Next(0, (int)Math.Pow(10, 9));
@@ -16,28 +22,28 @@ for (int i = 0; i < N; i++)
 {
     Console.WriteLine($"Введите {count + 1} Интервал");
     Console.WriteLine($"От");
-    array1[0, i] = Convert.ToInt32(Console.ReadLine());
+    x = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine($"До");
-    array1[1, i] = Convert.ToInt32(Console.ReadLine());
-    if (array1[1, i] < array1[0, i])
+    y = Convert.ToInt32(Console.ReadLine());
+    if (y < x|| x > Math.Pow(10, 9) || y > Math.Pow(10, 9) || x < 0 || y < 0)
     {
+        Console.WriteLine($"Ошибка ввода");
+        i--;
         count--;
-        Console.WriteLine($"Второе число должно быть больше первого");
+    }
+    else
+    {
+        do
+        {
+            if (array0[x] == 0) array0[x] = 1; else array0[x] = 0;
+            x++;
+        }
+        while (x < y);
+        x = -1; y = -1;
     }
     count++;
 }
-for (int i = 0; i < N; i++)
-{
-    count = array1[0, i];
-    do
-    {
-        if (array0[count] == 0) array0[count] = 1; else array0[count] = 0;
-        count++;
-    }
-    while (count < array1[1, i]);
-}
 count = 0;
-int temp = 0;
 foreach (int i in array0)
 {
     if (i == 0) count++;
